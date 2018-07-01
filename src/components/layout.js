8cfb4e369fa2be82b122d250e2efa930448a9427
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-07-01 00:19:49 -0400

    END of failed attempt at doing per-route-bundling

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,21 +3,19 @@ import React from 'react';
 // import { hot } from 'react-hot-loader'
 //
 // import Routes from 'react-static-routes'
-import { Router, Link } from '@reach/router';
+import { Link } from '@reach/router';
 
 // import './app.css'
 
 const Layout = ({ children }) => (
-  <Router>
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="about">About</Link>
-        <Link to="blog">Blog</Link>
-      </nav>
-      <div className="content">{children}</div>
-    </div>
-  </Router>
+  <div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="about">About</Link>
+      <Link to="blog">Blog</Link>
+    </nav>
+    <div className="content">{children}</div>
+  </div>
 );
 
 export default Layout;
