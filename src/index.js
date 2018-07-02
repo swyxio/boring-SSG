e02f4ac29cf48391e5dd5fb7b3ddff60e4666a94
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-07-02 05:44:33 -0400

    rewrite bigball for stability

--- src/index.js
+++ src/index.js
@@ -11,28 +11,32 @@ import Oops from './pages/404';
 import 'bulma/css/bulma.css';
 import './app.css';
 
-const App = ({ data }) => {
-  if (!data && typeof window !== 'undefined') {
-    data = window.BigBall; // TODO: i know this is so ugly. would love to refactor.
-    console.log('fromwindow', { data });
-  } else if (typeof window !== 'undefined') {
-    if (!data & !window.BigBallReload) {
-      window.BigBallReload = true
-      fetch("/BigBall.json")
-        .then(res => res.json())
-        .then(myJson => (window.BigBall = myJson))
-      return <Oops /> // no data, show an oops
+class App extends React.Component {
+  state = { toggle: false }
+  render() {
+    let { data } = this.props
+    if (!data) {
+      if (typeof window !== 'undefined') { // clientside
+        data = window.BigBall; // TODO: i know this is so ugly. would love to refactor.
+        console.log('fromwindow', { data });
+        if (!data) { // in case bigball isnt loaded
+          fetch("/BigBall.json")
+            .then(res => res.json())
+            .then(data => window.BigBall = data)
+            .then(() => this.setState(({ toggle }) => ({ toggle: !toggle })))
+        }
+      }
     }
+    if (!data) return <Oops /> // still no data, show an oops
+    return (
+      <Router>
+        <IndexPage path="/" posts={data.posts} />
+        <About path="/about" {...data.about[0]} />
+        <Products path="/products" {...data.products[0].data} />
+        <Blog path="/blog/:blogkey" posts={data.posts} />
+        <Oops default />
+      </Router>
+    );
   }
-  return (
-    <Router>
-      <IndexPage path="/" posts={data.posts} />
-      <About path="/about" {...data.about[0]} />
-      <Products path="/products" {...data.products[0].data} />
-      <Blog path="/blog/:blogkey" posts={data.posts} />
-      <Oops default />
-    </Router>
-  );
-};
-
+}
 export default App;
